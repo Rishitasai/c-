a9b2ded65d5c3b485f28f5b030b7b0947d691a91
Rishitasai <112751961+Rishitasai@users.noreply.github.com>
2022-09-07 10:51:50 +0530

    Add files via upload

---
+++ 192111304validname.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+string a,b;
+cout<<"enter user name\n";
+cin>>a;
+cout<<"renter the user name\n";
+cin>>b;
+if(a==b)
+{
+cout<<"valid user name";
+}
+else
+{
+cout<<"invalid user name";
+}
+return 0;
+}
